f28ac1f5db79e7a0b0ca0904def9ec58f9db5fb4
MauricioPenaranda <mpenaranda96@gmail.com>
2021-01-09 21:19:42 -0500

    V1.0

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PokemonSearch from './components/PokemonSearch';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PokemonSearch name="Mauro"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PokemonSearch.tsx
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+import User from '../interfaces/User.interface';
+
+interface SearchState {
+    error: boolean,
+    pokemon?: Pokemon
+}
+
+interface Pokemon {
+    numberOfAbilities: number,
+    baseExperience: number,
+    imageUrl: string,
+    name: string,
+}
+
+export class PokemonSearch extends Component<User, SearchState> {
+    pokemonRef: React.RefObject<HTMLInputElement>;
+
+    constructor(props: User) {
+        super(props);
+        this.state = {
+            error: false,
+            pokemon: null
+        }
+        this.pokemonRef = React.createRef();
+    }
+
+    onSearchClicked = () => {
+        const inputValue = this.pokemonRef.current?.value;
+        fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue.toLowerCase()}`).then(res => {
+            if (res.status != 200) {
+                this.setState({ error: true });
+                return;
+            }
+            res.json().then(data => {
+                this.setState({
+                    error: false,
+                    pokemon: {
+                        name: data.name.charAt(0).toUpperCase() + data.name.slice(1),
+                        numberOfAbilities: data.abilities.length,
+                        baseExperience: data.base_experience,
+                        imageUrl: data.sprites.front_default
+                    }
+                })
+            })
+        })
+    }
+
+    render() {
+        const {name: userName, numberOfPokemon} = this.props;
+        const { error, pokemon } = this.state;
+
+        let resultMarkup;
+
+        if (error) {
+            resultMarkup = <p>Pokemon not found, please try again</p>
+        }
+        else if (this.state.pokemon) {
+            resultMarkup = 
+                <div>
+                    <img src={pokemon.imageUrl} alt="pokemon" className="pokemon-image" />
+                    <p>
+                        {pokemon.name} has {pokemon.numberOfAbilities} 
+                        abilities and {pokemon.baseExperience} base experience poitns
+                    </p>
+                </div>
+        }
+
+        return (
+            <div>
+                <p>User: {userName} {numberOfPokemon && <span>has {numberOfPokemon} Pokemon</span>} </p>
+                <input type="text" ref={this.pokemonRef} />
+                <button onClick={this.onSearchClicked} className="my-button">
+                    Search
+                </button>
+                { resultMarkup }
+            </div>
+        )
+    }
+}
+
+export default PokemonSearch
